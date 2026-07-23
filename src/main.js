58445026b95fec1f6046713de381c573eb437f24
Volodymyr <vsirash@kajeet.com>
2026-07-23 12:14:19 +0300

    Fixed transitions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const menuEl = document.getElementById('mobile-menu');
+const overlayEl = document.getElementById('mobile-menu-overlay');
 const menuOpenButtons = document.querySelectorAll('.header-menu-icon');
 const menuCloseButton = document.querySelector('[data-menu-close]');
 
 function toggleMenu() {
   menuEl?.classList.toggle('is-open');
+  overlayEl?.classList.toggle('is-open');
 }
 
 menuCloseButton?.addEventListener('click', toggleMenu);
