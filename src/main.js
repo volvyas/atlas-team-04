8485bc1d71408310589994f4436ce3b94e186f30
Volodymyr <vsirash@kajeet.com>
2026-07-25 17:46:49 +0300

    Added email js service

--- src/main.js
+++ src/main.js
@@ -10,3 +10,34 @@ function toggleMenu() {
 
 menuCloseButton?.addEventListener('click', toggleMenu);
 menuOpenButtons.forEach(button => button.addEventListener('click', toggleMenu));
+
+
+emailjs.init({
+  publicKey: "5zeWkXDw85VBguJij",
+});
+
+document
+  .getElementById("application-form")
+  .addEventListener("submit", async e => {
+
+    e.preventDefault();
+    const selectedTeacher = e.currentTarget.querySelector(
+      'input[name="teacher"]:checked'
+    );
+
+    await emailjs.send(
+      "service_4zu15cg",
+      "template_4zq3z4q",
+      {
+        from_name: document.getElementById('id_name').value,
+        reply_to: document.getElementById('id_email').value,
+        name: document.getElementById('id_name').value,
+        email: document.getElementById('id_email').value,
+        teacher: selectedTeacher?.value || 'Не обрано',
+        phone: document.getElementById('id_phone').value,
+        comment: document.getElementById('id_comment').value,
+      }
+    );
+
+    alert("Лист відправлено!");
+  });
